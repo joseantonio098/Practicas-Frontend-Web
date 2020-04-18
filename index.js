fa0b8bacaa0acdbddd3020b51d4355f2740b7e8a
Jose Valencia <joseantonio9850@hotmail.com>
2020-04-18 13:37:41 -0500

    JS++

---
+++ index.js
@@ -0,0 +1,9 @@
+//------------> Arreglos con objetos
+
+const autos = [
+    {modelo: 'Mustang', motor: 6.2},
+    {modelo: 'Camaro', motor: 6.1},
+    {modelo: 'Challenger', motor: 6.3}
+]
+
+console.log(autos[0].motor);
---
+++ js-MásFunciones/masFunciones.js
@@ -0,0 +1,37 @@
+
+//................................... MÉTODOS Y FUNCIONES ADICIONALES DE JS ..............................
+
+
+ //-------------------- Convertir Números y Textos
+let numero= '100';
+let numero2= 299.323;
+let edad= 21;
+
+Number(numero);  // Convierte texto a número
+edad.toFixed(2); // Convierte un número a número exacto , (2) ---> es el Número de decimales --> (299.32)
+
+String(edad);  // Convierte número a texto
+edad.toString(); // Convierte número a texto
+
+
+//-------------------- Arreglos
+let semana  = new Array('lunes','martes','miercoles','jueves','viernes');
+let numeros = [3,6,1,5,70,10,30,40,20];
+
+semana.indexOf('miercoles');  // Indica la posición de un elemento en el array ---> (3)
+semana.slice(3,4); // Elimina el elemento de un array ---> elimina desde la posición 3 al 4 (2 elementos)
+
+numero.sort(function(a,b){
+    return a-b   // Permite ordenar Números según su valor  ----> (1,3,5,6,10,20,30,40,70)
+});
+
+// *Arreglos con objetos
+const autos = [
+    {modelo: 'Mustang', motor: 6.2},
+    {modelo: 'Camaro', motor: 6.1},
+    {modelo: 'Challenger', motor: 6.3}
+]
+console.log(autos[0].motor); //Accedemos al array autos ----> posición(0) ------> propiedad(motor)
+
+
+
