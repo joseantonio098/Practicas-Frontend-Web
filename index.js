e4c8aeea3e974462da415f9d21fe028cd0c35a5d
Jose Valencia <Joseantoniovalen1@gmail.com>
2020-10-16 20:29:40 -0500

    prácticas de js

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const Mascota = {
+    nombre : 'Tiger',
+    sexo : 'Macho',
+    edad : 2,
+    raza : {
 
+    }
+}
 
+const {nombre,sexo,edad} = Mascota;
+console.log(`Mi mascota se llama ${nombre}, el es un ${sexo} y tiene ${edad} años de edad`);
