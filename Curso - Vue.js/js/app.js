6caed250d2ab9f6fb3501a30c1abe595a88d916f
joseantonio098 <joseantoniovalen1@gmail.com>
2021-01-27 23:28:12 -0500

    Vue.js 27/01/21

--- Curso - Vue.js/js/app.js
+++ Curso - Vue.js/js/app.js
@@ -4,9 +4,31 @@ const app = Vue.createApp({ //Versión 3
             titulo: `Saludos desde Vue.js!`,
             nombre: `Jose Valencia`,
             cantidad: 10,
+            deportes: ['ajedrez','natación','futbol','voley'],
+            habilitar: false,
+            colorTexto: 'text-warning',
+            subTitulo: 'Welcome a vue!!',
             ruta: 'https://www.youtube.com/channel/UCeBvCHoZXt6uGMo33BPH7oQ',
         }
     },
+    methods: {
+        agregarCantidad(){
+            this.cantidad ++;
+        },
+        disminuirCantidad(valor){
+            if( this.cantidad == 0 ){
+                alert('Cantidad Vacía');
+                this.habilitar = true;
+                return;
+            }
+            this.cantidad = this.cantidad - valor;
+        }
+    },
+    computed: {
+        txtMayuscula(){
+            return this.subTitulo.toUpperCase();
+        }
+    }
 })
 
 const mountedApp = app.mount('#app'); //Relacionamos el id="app" con la Instancia
